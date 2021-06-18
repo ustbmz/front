0f17c2e7ceb270f1d31f17d69746b09bf1d3ef15
max2h3 <max2h3@126.com>
2021-06-18 17:34:05 +0800

    修改footer样式

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="fly-footer">
     <p>
-      <a href="http://fly.layui.com/" target="_blank">Imooc社区</a> 2017 &copy;
-      <a href="http://www.layui.com/" target="_blank">Imooc.com 出品</a>
+      <a href="http://fly.layui.com/" target="_blank">大钱端社区</a> 2021 &copy;
+      <a href="http://www.ustbmz.com/" target="_blank">Max2H3</a>
     </p>
     <p>
-      <a href="http://fly.layui.com/jie/3147/" target="_blank">付费计划</a>
-      <a href="http://www.layui.com/template/fly/" target="_blank">获取Fly社区模版</a>
-      <a href="http://fly.layui.com/jie/2461/" target="_blank">微信公众号</a>
+      <a href="/" target="_blank">付费计划</a>
+      <a href="/" target="_blank">获取Fly社区模版</a>
+      <a href="/" target="_blank">微信公众号</a>
     </p>
   </div>
 </template>
