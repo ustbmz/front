20c9510f19f4630e4e277d25f074507bf9d6b769
cnmz <cnmz@msn.com>
2021-11-17 09:12:57 +0800

    删除多余底部内容

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="fly-footer">
-    <p>
-      <a href="http://fly.layui.com/" target="_blank">大钱端社区</a> 2021 &copy;
-      <a href="http://www.ustbmz.com/" target="_blank">Max2H3</a>
-    </p>
-    <p>
-      <a href="/" target="_blank">付费计划</a>
-      <a href="/" target="_blank">获取Fly社区模版</a>
-      <a href="/" target="_blank">微信公众号</a>
-    </p>
+    <div class="bt-footer"><p>©2021 ustbmz. All rights reserved</p></div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Footer'
+  name: 'Footer',
 }
 </script>
 
 <style lang="scss" scoped>
+.fly-footer {
+  min-height: 100px;
+}
 </style>
