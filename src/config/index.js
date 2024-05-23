218c8b4d1d928bcf890048a61b915d74182971dd
ustbmz <cnmz@msn.com>
2024-05-23 08:58:56 +0800

    ddd

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   baseUrl: {
-    dev: 'http://localhost:3000',
-    pro: 'https://api.ustbmz.com',
+    dev: 'http://121.40.150.70:3000',
+    pro: 'http://127.0.0.1:3000',
   },
 }
